c01606a7c36b4e59fde8a297b3e2b986a1843fe3
seize-a <mallard.damien@gmail.com>
2025-11-11 21:12:08 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,21 @@
-const CACHE_NAME = "pwa-demo-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192x192.png",
-  "./icons/icon-512x512.png"
-];
+const CACHE_NAME = '16a-v1';
+const FILES = ['./','./index.html','./manifest.json','./icons/icon-192x192.png','./icons/icon-512x512.png'];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then((res) => {
+      return res || fetch(evt.request);
+    })
   );
 });
