44c42ebd173e9ec922a6b710ec2fdf7f6bf9512f
seize-a <mallard.damien@gmail.com>
2025-11-13 00:20:50 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='16a-cache-v1';
+const CACHE_NAME='16a-cache-v2';
 const FILES=['/16a/','/16a/index.html','/16a/manifest.json','/16a/icon-192x192.png','/16a/icon-512x512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(clients.claim());});
