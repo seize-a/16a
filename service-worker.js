14707a661d8c518e4d84fa275a1ecaee6fed88e9
seize-a <mallard.damien@gmail.com>
2025-11-12 23:26:14 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,5 @@
-const CACHE_NAME = '16a-v2';
-const FILES = ['/16a/','/16a/index.html','/16a/manifest.json','/16a/icon-192x192.png','/16a/icon-512x512.png'];
-
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(
-    caches.match(evt.request).then(res => res || fetch(evt.request))
-  );
-});
+const CACHE_NAME='16a-cache-v1';
+const FILES=['/16a/','/16a/index.html','/16a/manifest.json','/16a/icon-192x192.png','/16a/icon-512x512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
